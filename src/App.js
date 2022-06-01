75c33168bfe5d487166cd281ffb4de68c939cfb9
Omar Suriel <omarsuriel@icloud.com>
2022-06-01 10:50:51 -0400

    ADDED HEADER

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
+        <h1 style={{backgroundColor: 'red'}}>This is the header</h1>
+      </header>
         <img src='https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png' className="App-logo" alt="logo" />
         <h1>
           Git practice
         </h1>
-      </header>
     </div>
   );
 }
