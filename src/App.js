30121ceb7c728be21ee53bf3a9ce9ce4db258da4
Omar Suriel <omarsuriel@icloud.com>
2022-06-01 11:00:43 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <img src='https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png' className="App-logo" alt="logo" />
         <h1>
           Git practice
         </h1>
-      </header>
+
+      <footer style={{background: 'red'}}><h2>This is the footer of the page</h2></footer>
     </div>
   );
 }
